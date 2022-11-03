9be106ced4b1aa2d03cd31223a158bbda21caf43
vinayakgarole <vinayakgarole10@gmail.com>
2022-11-03 15:02:25 +0530

    added: Find Reverse Number

---
+++ src/com/bridgelabz/ReverseNumber.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+
+        int n, r;
+        System.out.println("Enter any Number: ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        while (n > 0) {
+            r = n % 10;
+            System.out.print(r);
+            n = n / 10;
+        }
+    }
+}
